9e4f2bbd1d99722c7d5e63b883a01c0bf29fbfab
Teja Kumar Muppala <142835834+tejakumarsai6309@users.noreply.github.com>
2023-11-28 16:52:50 -0600

    updated create,delete&update features

--- models/vehicle.js
+++ models/vehicle.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose")
 const vehicleSchema = mongoose.Schema({
 Brand: String,
 Color: String,
-Year: Number
-})
+Year:  {
+    type: Number,
+    min: [1900, "Year must be greater than or equal to 1900"],
+    max: [new Date().getFullYear(), "Year cannot be greater than the current year"]
+  }
+});
 module.exports = mongoose.model("vehicle",vehicleSchema)
+
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const vehicle_controlers= require('../controllers/vehicle');
 var router = express.Router();
+var passport = require('passport');
 /* GET vehicles */
 router.get('/', vehicle_controlers.vehicle_view_all_Page );
 
@@ -10,8 +11,22 @@ router.get('/detail', vehicle_controlers.vehicle_view_one_Page);
 /* GET create vehicle page */
 router.get('/create', vehicle_controlers.vehicle_create_Page);
 
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+res.redirect("/login");
+}
+/* GET update costume page */
+router.get('/update', secured,vehicle_controlers.vehicle_update_Page);
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/');
+    });
+
 /* GET create update page */
-router.get('/update', vehicle_controlers.vehicle_update_Page);
+//router.get('/update', vehicle_controlers.vehicle_update_Page);
 
 /* GET delete vehicle page */
 router.get('/delete', vehicle_controlers.vehicle_delete_Page);
